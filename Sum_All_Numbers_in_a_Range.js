6d9ddad2e50695a0bf89abf1b119eaa47a9aa178
Alex Zhang <alexgzhang92@gmail.com>
2019-05-19 20:11:34 -0400

    Update Sum_All_Numbers_in_a_Range.js

--- Sum_All_Numbers_in_a_Range.js
+++ Sum_All_Numbers_in_a_Range.js
@@ -1,4 +1,5 @@
-We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them.
+Instructions:
+We will pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them.
 
 The lowest number will not always come first.
 
